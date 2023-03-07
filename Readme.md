ec79b3d316887d0d07132124f374b07adcb97fca
Stevens <shanes@belpark.sun.ac.za>
2023-03-07 15:45:48 +0200

    update

---
+++ Readme.md
@@ -0,0 +1,4 @@
+## VIDEO TUT https://www.youtube.com/watch?v=kng-mJJby8g
+
+## Install Flask which is a lightweight python framework
+# Run " python3 -m pip install flask " to install the module/framework
---
+++ app.py
@@ -0,0 +1,19 @@
+# BEFORE YOU START RUN " python3 -m pip install flask " TO INSTALL THE FLASK FRAMEWORK
+
+# Import module
+from flask import Flask
+
+# initialize application of the app
+app = Flask(__name__)
+
+
+# (DOING THIS WORKS HOWEVER IT MAKES THIS FILE LOOK CLUTTERED THEREFORE ALL ROUTES ARE PLACED INTO THE VIEW.PY FILE )
+# website path
+# @app.route("/")
+# def home():
+#     return "this is the home page"
+
+
+# run website
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
